51d1276f65ae5785273d82751531927818277803
0xiammatrixx <davidotom50@gmail.com>
2023-09-13 02:57:45 -0700

    putxhar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,10 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - Entry point
  *
  * Return: 0 (Success)
  */
 int main(void){
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	write(1,"_puchar\n")
 	return (0);
 }
